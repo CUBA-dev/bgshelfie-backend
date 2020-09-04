f6145033d8f6e293b0a30f61a252fbf49d8f541f
Sayuri Arake <sayurijoazeiro@mti.mt.gov.br>
2020-09-03 20:13:35 -0400

    CRUD do domínio

--- cuba.dev/src/server.js
+++ cuba.dev/src/server.js
@@ -85,3 +85,69 @@ app.route("/mecanica/delete/:id").get((req, res) => {
     res.redirect("/mecanica/show")
   })
 })
+
+app
+  .route("/dominio") 
+  .get(function (req, res) {
+    const cursor = db.collection("dominio").find()
+    res.render("dominio/index.ejs")
+  })
+
+  .post((req, res) => {
+    db.collection("dominio").save(req.body, (err, result) => {
+      if (err) return console.log(err);
+      console.log("Salvo no Banco de Dados");
+      res.redirect("dominio/show");
+    });
+  })
+
+app.route("/dominio/show").get((req, res) => {
+  db.collection("dominio")
+    .find()
+    .toArray((err, results) => {
+      if (err) return console.log(err)
+      res.render("dominio/show.ejs", { dominio: results })
+    })
+})
+
+app
+  .route("/dominio/edit/:id")
+  .get((req, res) => {
+    var id = req.params.id
+
+    db.collection("dominio")
+      .find(ObjectId(id))
+      .toArray((err, result) => {
+        if (err) return res.send(err)
+        res.render("dominio/edit.ejs", { dominio: result })
+      })
+  })
+  .post((req, res) => {
+    var id = req.params.id
+    var desc = req.body.desc
+
+    db.collection("dominio").updateOne(
+      { _id: ObjectId(id) },
+      {
+        $set: {
+          desc: desc,
+        },
+      },
+      (err, result) => {
+          console.log(err)
+        if (err) return res.send(err)
+        res.redirect("/dominio/show")
+        console.log("Atualizado no Banco de Dados")
+      }
+    )
+  })
+
+app.route("/dominio/delete/:id").get((req, res) => {
+  var id = req.params.id
+
+  db.collection("dominio").deleteOne({ _id: ObjectId(id) }, (err, result) => {
+    if (err) return res.send(500, err)
+    console.log("Deletado do Banco de Dados!")
+    res.redirect("/dominio/show")
+  })
+})
