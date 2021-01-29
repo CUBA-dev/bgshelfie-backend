33ad0dd1903e71e22466d1f704a1417e2057a897
Thais <tfb.bueno@gmail.com>
2021-01-28 20:48:05 -0400

    Commit teste

--- cuba.dev/server.js
+++ cuba.dev/server.js
@@ -4,7 +4,8 @@ var http = require('http');
 var app = require('./config/express')();
 require("./config/database.js")(process.env.URI_BD);
 
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), function () {
+    
     console.log('Express coffee Server escutando na porta '+ app.get('port'));
 });
 
