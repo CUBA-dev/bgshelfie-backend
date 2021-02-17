af19b67e3d7f134f57d5781359446a2416662271
Sayuri Arake <sayurijoazeiro@mti.mt.gov.br>
2021-02-16 21:35:00 -0400

    Revert "feat: :art: inclusão de campos em usuário, criação de api para cidades."
    This reverts commit 5b2d4bd40d88258f60e6df8bbf4b08709cb9b2ce.

--- cuba.dev/app/models/usuario.js
+++ cuba.dev/app/models/usuario.js
@@ -3,57 +3,18 @@ const mongoose = require("mongoose");
 module.exports = () => {
   const UsuarioSchema = mongoose.Schema(
     {
-      nome: {
-         type: String, 
-         required: true,
-         unique: false, 
-         validate:{
-          notEmpty: {
-            msg: 'Nome é de preenchimento obrigatório.'
-          },
-         },
-        },
+      nome: { type: String, required: true, unique: false },
       apelido: { type: String, required: false, unique: false },
-      email: { 
-        type: String, 
-        required: true, 
-        unique: true,
-        validate: {
-          notEmpty: {
-            msg: 'E-mail é de preenchimento obrigatório.'
-          },
-          isEmail: {
-              msg: 'Digite um e-mail válido.'
-          }
-        } 
-      },
-      senha: { 
-        type: String,
-        required: true,
-        validate:{
-          notEmpty: {
-            msg: 'Senha é de preenchimento obrigatório.'
-          },
-         },
-       },
-      cidade: { 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: "Cidade"
-      },
-      telefone: { type: String, required: false, unique: true, },
-      dataNascimento: { type: Date, required: false, unique: false },
+      email: { type: String, required: true, unique: true },
+      cidade: { type: String, required: false, unique: false },
+      telefone: { type: String, required: false, unique: true },
+      dataNascimento: { type: Date, required: false, unique: true },
 
     },
     {
       timestamps: true,
     }
-    /*TO DO
-
-    - Imagem
-    
-    */
   );
-
 
   return mongoose.model("Usuario", UsuarioSchema);
 };
